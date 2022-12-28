7804f89b242cec75a360663c762cabc7cc976c3a
shaho1090 <shaho.parvini@gmail.com>
2022-12-28 15:45:13 +0330

    add useful mothod for getting vendor id

--- app/src/DataFixtures/AppFixtures.php
+++ app/src/DataFixtures/AppFixtures.php
@@ -2,16 +2,39 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Vendor;
+use App\Repository\VendorRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    private VendorRepository $vendorRepository;
+
+    public function __construct(VendorRepository $vendorRepository)
+    {
+        $this->vendorRepository = $vendorRepository;
+    }
+
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
 
         $manager->flush();
+    }
+
+    public function vendorId()
+    {
+        $vendors = $this->vendorRepository->findAll();
+
+        $collection = new ArrayCollection($vendors);
+
+        $vendors = $collection->toArray();
+
+        $vendor = $vendors[array_rand($vendors)];
+
+        return $vendor['id'];
     }
 }
